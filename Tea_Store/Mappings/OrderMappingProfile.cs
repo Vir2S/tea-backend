89f982c1862dc0c803be234bdd66b065923c79e0
Daniil <Zest005@gmail.com>
2024-10-05 14:39:45 +0300

    Added mapping to OrderController

--- Tea_Store/Mappings/OrderMappingProfile.cs
+++ Tea_Store/Mappings/OrderMappingProfile.cs
@@ -8,11 +8,21 @@ namespace Tea_Store.Mappings
     {
         public OrderMappingProfile()
         {
+            // OrderCreateDTO - Order
             CreateMap<OrderCreateDTO, Order>()
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateTime.Now))
                 .ForMember(dest => dest.Created, opt => opt.MapFrom(src => DateTime.Now))
                 .ForMember(dest => dest.Updated, opt => opt.MapFrom(src => DateTime.Now))
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => "Pending"));
+
+            // Order - OrderViewDTO
+            CreateMap<Order, OrderViewDTO>()
+            .ForMember(dest => dest.Teas, opt => opt.MapFrom(src => src.OrderTeas.Select(ot => new TeaDTO
+            {
+                Id = ot.Tea.Id,
+                Title = ot.Tea.Title,
+                Price = ot.Tea.Price
+            }).ToList()));
         }
     }
 }
