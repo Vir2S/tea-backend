75a6463b486f3d57c1c6003af9021679f997fa6f
DESKTOP-IBJNLFM\MikeIt <mixaylo.skv@gmail.com>
2024-11-08 13:07:38 +0200

    merge

---
+++ ViewModels/CartController/CartItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.CartController
+{
+    public class CartItemViewModel
+    {
+        public int TeaId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
